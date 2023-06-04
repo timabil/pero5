1416c75b39fcfb93c463e2e50895e90f4494337e
unknown <gaidenkoivan1@gmail.com>
2023-06-04 16:15:48 +0300

    add retry

--- create_contract.js
+++ create_contract.js
@@ -29,6 +29,11 @@ async function deployErc20(signer) {
         console.log("future token address:", defaultAbiCoder.decode(["address"], contractAddress), "deploying..");
         contractAddress = defaultAbiCoder.decode(["address"], contractAddress)
     } catch (e) {
+        if (e.reason.includes("missing")) {
+            console.log("нода послала нахуй, адихаем 2 минуты");
+            await sleep(120);
+            return await deployErc20(signer);
+        }
         console.log(e.reason);
         console.log("cant get create2 address");
     }
@@ -40,6 +45,11 @@ async function deployErc20(signer) {
         await writeContractToFile(signer.address + ":" + contractAddress);
         contractAddress = contractAddress[0];
     } catch (e) {
+        if (e.reason.includes("missing")) {
+            console.log("нода послала нахуй, адихаем 2 минуты");
+            await sleep(120);
+            return await deployErc20(signer);
+        }
         console.log(e.reason);
         console.log("error on erc20 deploy");
     }
@@ -48,18 +58,22 @@ async function deployErc20(signer) {
 
 async function openMint(signer, token) {
     let data = `0xac9650d800000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000084938e3d7b00000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000037697066733a2f2f516d58766d4255314e527464736b74687861325844444c6a556b6931444c446944534d4d595a3243516a57524b732f300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001e474bc7db70000000000000000000000000000000000000000000000000000000000000040000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000006477066fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff0000000000000000000000000000000000000000000000000000000000000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee00000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000037697066733a2f2f516d5675393865637a5a52705359634633554b5952446b48734d32524d515236324b55596d6b32395544625754502f3000000000000000000000000000000000000000000000000000000000000000000000000000`;
-
-    let tx = {
-        to: token,
-        data: data,
-        gasPrice: (await getGasPrice()).mul("2"),
-    }
-    let openMint;
     try {
+        let tx = {
+            to: token,
+            data: data,
+            gasPrice: (await getGasPrice()).mul("2"),
+        }
+        let openMint;
         openMint = await signer.sendTransaction(tx);
         console.log("opened mint to public", "https://explorer.goerli.linea.build/tx/" + openMint.hash);
         return true;
     } catch (e) {
+        if (e.reason.includes("missing")) {
+            console.log("нода послала нахуй, адихаем 2 минуты");
+            await sleep(120);
+            return await openMint(signer, token);
+        }
         console.log(e.reason);
         console.log("error on open mint to public");
         return false;
@@ -69,17 +83,22 @@ async function openMint(signer, token) {
 async function mintToken(signer, token) {
     let data = `0x84bb1e42000000000000000000000000${signer.address.slice(2)}0000000000000000000000000000000000000000000000008ac7230489e80000000000000000000000000000eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000c000000000000000000000000000000000000000000000000000000000000001800000000000000000000000000000000000000000000000000000000000000080ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`
 
-    let tx = {
-        data: data,
-        to: token,
-        gasPrice: (await getGasPrice()).mul("2"),
-    }
-    let mintTx;
     try {
+        let tx = {
+            data: data,
+            to: token,
+            gasPrice: (await getGasPrice()).mul("2"),
+        }
+        let mintTx;
         mintTx = await signer.sendTransaction(tx);
         console.log("minted 10 tokens", "https://explorer.goerli.linea.build/tx/" + mintTx.hash);
         return true;
     } catch (e) {
+        if (e.reason.includes("missing")) {
+            console.log("нода послала нахуй, адихаем 2 минуты");
+            await sleep(120);
+            return await mintToken(signer, token);
+        }
         console.log(e.reason);
         console.log("error on minting 10 tokens");
         return false;
@@ -88,17 +107,22 @@ async function mintToken(signer, token) {
 async function transferToken(signer, token) {
     let data = `0xa9059cbb000000000000000000000000630900fb257fafef02491368062d50d6677d9d750000000000000000000000000000000000000000000000000de0b6b3a7640000`
 
-    let tx = {
-        data: data,
-        to: token,
-        gasPrice: (await getGasPrice()).mul("2"),
-    }
-    let transfer;
     try {
+        let tx = {
+            data: data,
+            to: token,
+            gasPrice: (await getGasPrice()).mul("2"),
+        }
+        let transfer;
         transfer = await signer.sendTransaction(tx);
         console.log("sent 1 token", "https://explorer.goerli.linea.build/tx/" + transfer.hash);
         return true;
     } catch (e) {
+        if (e.reason.includes("missing")) {
+            console.log("нода послала нахуй, адихаем 2 минуты");
+            await sleep(120);
+            return await transferToken(signer, token);
+        }
         console.log(e.reason);
         console.log("error on sending 1 token");
         return false;
@@ -109,7 +133,6 @@ async function getGasPrice() {
     let price = fee.gasPrice;
     return price;
 }
-await getGasPrice();
 
 export {
     deployErc20,
